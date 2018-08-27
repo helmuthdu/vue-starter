3b5bd3369d3d6ea1008b43fa0312be2f06a89231
Helmuth Saatkamp <helmuthdu@gmail.com>
2018-08-27 17:44:08 -0300

    feat: added ability to do chain requests in Http decorator;

--- src/decorators/http.decorator.ts
+++ src/decorators/http.decorator.ts
@@ -3,18 +3,22 @@ import { beforeMethod } from 'kaop-ts';
 
 type httpRequestMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
-export const Http = (method: httpRequestMethod, ...args: any[]) => {
-  return beforeMethod((meta: any) => {
-    const [params] = meta.args;
+const httpDecorator = Symbol.for('Http');
+
+export const Http = (method: httpRequestMethod, ...args: any[]) =>
+  beforeMethod((meta: any) => {
     // @ts-ignore
     axios[method](...args.map((arg: any) => (typeof arg === 'function' ? arg(meta.scope) : arg)))
       .then((res: AxiosResponse) => {
-        meta.args = [params, null, res.data];
+        if (meta.args.length && meta.args[meta.args.length - 1] === httpDecorator) {
+          meta.args = [meta.args[0], meta.args[1], [meta.args[2], res.data]];
+        } else {
+          meta.args = [meta.args, null, res.data, httpDecorator];
+        }
         meta.commit();
       })
       .catch((err: AxiosError) => {
-        meta.args = [params, err, null];
+        meta.args = [meta.args, err, null, httpDecorator];
         meta.commit();
       });
   });
-};
