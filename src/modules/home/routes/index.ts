85db18de69b01557df1057a5e5b4edba40373b71
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-04-05 21:00:24 +0200

    feat: new home module

---
+++ src/modules/home/routes/index.ts
@@ -0,0 +1,23 @@
+export enum HOME_ROUTES {
+  HOME = 'home-route',
+  ABOUT = 'about-route'
+}
+
+export const routes = [
+  {
+    path: '/',
+    component: () => import('@/layouts/default.layout.vue'),
+    children: [
+      {
+        path: '/about',
+        name: HOME_ROUTES.ABOUT,
+        component: () => import('@/modules/home/routes/about/about.route.vue')
+      },
+      {
+        path: '',
+        name: HOME_ROUTES.HOME,
+        component: () => import('@/modules/home/routes/home/home.route.vue')
+      }
+    ]
+  }
+];
---
+++ src/routes/not-found/not-found.route.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <blockquote class="blockquote">
+        You are here!
+        <footer>
+          <small> <em>&mdash;But nothing found for you #404</em> </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class AboutRoute extends Vue {}
+</script>
