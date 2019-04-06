31f7398aaf2e1a9f907fe91359764999f08c09bf
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-04-06 23:05:45 +0200

    chore: minor changes

--- src/modules.ts
+++ src/modules.ts
@@ -1,7 +1,7 @@
 import * as authModule from '@/modules/auth';
-import * as homeModule from '@/modules/root';
+import * as rootModule from '@/modules/root';
 
 export type State = authModule.State;
 
 export const stores = [authModule.stores];
-export const routes = [authModule.routes, homeModule.routes];
+export const routes = [authModule.routes, rootModule.routes];
