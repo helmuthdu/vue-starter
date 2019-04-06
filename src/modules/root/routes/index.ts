2864b3db505e1cedda401dcb1cf12e703e0176a6
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-04-06 22:56:10 +0200

    chore: renamed module name from main to root

--- src/modules/root/routes/index.ts
+++ src/modules/root/routes/index.ts
@@ -1,4 +1,4 @@
-export enum HOME_ROUTES {
+export enum ROOT_ROUTES {
   HOME = 'home-route',
   ABOUT = 'about-route'
 }
@@ -10,12 +10,12 @@ export const routes = [
     children: [
       {
         path: '/about',
-        name: HOME_ROUTES.ABOUT,
+        name: ROOT_ROUTES.ABOUT,
         component: () => import('./about/about.route.vue')
       },
       {
         path: '',
-        name: HOME_ROUTES.HOME,
+        name: ROOT_ROUTES.HOME,
         component: () => import('./home/home.route.vue')
       }
     ]
