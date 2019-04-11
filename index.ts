952790c1d42fa643da7163ee619271aaa00d0645
h.saatkamp <helmuth.saatkamp@leverton.ai>
2019-04-11 14:33:38 +0200

    chore: code cleanup

--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-import * as authModule from './src/modules/auth';
-import * as rootModule from './src/modules/root';
-
-export type State = authModule.State;
-
-export const routes = [authModule.routes, rootModule.routes];
-export const stores = [authModule.stores];
-
-export default {
-  routes,
-  stores
-};
+export * from './src/modules';
