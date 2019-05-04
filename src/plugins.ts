40c38cbc6c6aced7c8bd0f542d8fd96b2242dd3c
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-05-04 08:04:17 +0200

    chore: update packages.json

--- src/plugins.ts
+++ src/plugins.ts
@@ -3,8 +3,6 @@ import VeeValidate from 'vee-validate';
 import Vue from 'vue';
 import VueRx from 'vue-rx';
 
-import '@mdi/font/css/materialdesignicons.css';
-
 Vue.use(Buefy);
 Vue.use(VeeValidate);
 Vue.use(VueRx);
