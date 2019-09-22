c3408891b27564c401284b3084af48d1f4a9a861
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-09-22 10:17:12 +0200

    chore: update package.json

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import * as VeeValidate from 'vee-validate';
+import VueCompositionApi from '@vue/composition-api';
 import VueRx from 'vue-rx';
 import Vuetify from 'vuetify/lib';
 
+Vue.use(VueCompositionApi);
 Vue.use(VueRx);
 Vue.use(Vuetify);
 Vue.use(VeeValidate);
