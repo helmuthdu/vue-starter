68c3fb5dc5ecbb8653043a76153b593a834170d8
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-04-13 18:53:01 +0200

    chore: update package.json

--- src/modules/root/layouts/default.layout.vue
+++ src/modules/root/layouts/default.layout.vue
@@ -54,7 +54,7 @@
 
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+      <v-toolbar-title style="width: 300px;" class="ml-0 pl-4">
         <span class="hidden-sm-and-down">Google Contacts</span>
       </v-toolbar-title>
       <v-text-field
