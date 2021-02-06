95c9ade04a8fc4582c55019db86c40ad903ec3ff
helmuthdu <helmuthdu@gmail.com>
2021-02-06 09:41:23 +0100

    chore: minor changes

--- src/decorators/http.decorator.ts
+++ src/decorators/http.decorator.ts
@@ -6,20 +6,22 @@ type httpRequestMethod = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 const httpDecorator = Symbol.for('Http');
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const Http = <T = any>(method: httpRequestMethod, ...args: any[]) =>
-  beforeMethod((meta: Metadata<AxiosResponse<T>>) =>
-    (http[method] as any)(...args.map((arg: any) => (typeof arg === 'function' ? arg(meta.scope) : arg)))
-      .then((res: AxiosResponse<T>) => {
-        if (meta.args.length && meta.args[meta.args.length - 1] === httpDecorator) {
-          const data = Array.isArray(meta.args[2]) ? [...meta.args[2], res] : [meta.args[2], res];
-          meta.args = [meta.args[0], meta.args[1], data];
-        } else {
-          meta.args = [meta.args, null, res, httpDecorator];
-        }
-        meta.commit();
-      })
-      .catch((err: AxiosError) => {
-        meta.args = [meta.args, err, null, httpDecorator];
-        meta.commit();
-      })
+  beforeMethod(
+    (meta: Metadata<AxiosResponse<T>>): AxiosResponse<T> =>
+      (http[method] as any)(...args.map((arg: any) => (typeof arg === 'function' ? arg(meta.scope) : arg)))
+        .then((res: AxiosResponse<T>) => {
+          if (meta.args.length && meta.args[meta.args.length - 1] === httpDecorator) {
+            const data = Array.isArray(meta.args[2]) ? [...meta.args[2], res] : [meta.args[2], res];
+            meta.args = [meta.args[0], meta.args[1], data];
+          } else {
+            meta.args = [meta.args, null, res, httpDecorator];
+          }
+          meta.commit();
+        })
+        .catch((err: AxiosError) => {
+          meta.args = [meta.args, err, null, httpDecorator];
+          meta.commit();
+        })
   );
