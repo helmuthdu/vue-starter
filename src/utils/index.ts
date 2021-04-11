fe42a0a02d9015757e188bb534f8b9dbbf8f0e1c
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-04-11 20:01:39 +0200

    feat: new storage utils

--- src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './http.util';
-export * from './search.util';
-export * from './webworker.util';
---
+++ src/utils/storage.util.ts
@@ -0,0 +1,49 @@
+import Logger from '@/utils/logger.util';
+
+const generatePrefix = (): string => {
+  const appName = process.env.APP_NAME ?? '_app';
+  const environment = process.env.NODE_ENV ?? 'development';
+  const suffix = `-${environment.substr(0, 3)}`;
+  return `${appName.name}_${suffix}`;
+};
+
+export const removeStorageItem = (key: string): void => {
+  try {
+    localStorage.removeItem(`${generatePrefix()}-${key}`);
+    sessionStorage.removeItem(`${generatePrefix()}-${key}`);
+  } catch {
+    Logger.error('Failed to remove data from storage');
+  }
+};
+
+export const setStorageItem = <T>(key: string, value?: T, session = false): void => {
+  try {
+    const storage = session ? sessionStorage : localStorage;
+    const itemKey = `${generatePrefix()}-${key}`;
+
+    if (value === undefined) {
+      storage.removeItem(itemKey);
+    } else {
+      storage.setItem(itemKey, typeof value !== 'string' ? JSON.stringify(value) : value);
+    }
+  } catch {
+    Logger.error('Failed to save data to storage');
+  }
+};
+
+export const getStorageItem = <T>(key: string, defaultValue?: T): T | undefined => {
+  try {
+    const prefix = generatePrefix();
+    const item = sessionStorage.getItem(`${prefix}_${key}`) ?? localStorage.getItem(`${prefix}-${key}`);
+    return typeof item === 'string' ? JSON.parse(item) : defaultValue;
+  } catch {
+    Logger.error('Failed to get data from storage');
+    return defaultValue;
+  }
+};
+
+export default {
+  get: getStorageItem,
+  set: setStorageItem,
+  remove: removeStorageItem
+};
