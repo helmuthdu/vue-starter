a49f68fb27ec543d6d1a4c027804402c30a18965
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-04-13 09:04:29 +0200

    fix: failed to update and initialise correct from localstorage

--- src/hooks/storage.hook.ts
+++ src/hooks/storage.hook.ts
@@ -3,18 +3,23 @@ import { getStorageItem, setStorageItem } from '@/utils/storage.util';
 
 export const useStorage = <T>(key: string, initialValue?: T, session = false): Ref<UnwrapRef<T> | undefined> => {
   const getItem = () => {
-    const item = getStorageItem<T>(key, initialValue);
-    if (!item) {
+    const item = getStorageItem<T>(key);
+    if (!item && initialValue) {
       setStorageItem(key, initialValue, session);
+      return initialValue;
     }
     return item;
   };
 
   const storage = ref<T | undefined>(getItem());
 
-  watch(storage, (data: any) => {
-    setStorageItem(key, data);
-  });
+  watch(
+    storage,
+    (state: any) => {
+      setStorageItem(key, state);
+    },
+    { deep: true }
+  );
 
   return storage;
 };
