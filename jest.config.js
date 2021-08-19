d4c5403dde55e518e10b927c80b8ce2abec9b687
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-08-19 11:47:14 +0200

    fix: jest test failing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 module.exports = {
+  globals: {
+    'vue-jest': {
+      babelConfig: true
+    }
+  },
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['./tests/unit/setupTest.ts'],
   moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
   testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest',
+    '^.+\\.(ts|tsx|js|jsx)?$': 'babel-jest',
     '^.+\\.vue$': 'vue-jest'
   },
   moduleNameMapper: {
