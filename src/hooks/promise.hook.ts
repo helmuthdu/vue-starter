bc1f3f3890ecfe1b05e51f76b106dfa74d9dc592
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-09-29 19:08:00 +0200

    feat: new promise hook

---
+++ src/hooks/promise.hook.ts
@@ -0,0 +1,20 @@
+import { ref, Ref } from 'vue';
+
+export const usePromise = <T>(fn: (...args: any) => Promise<T>, defaultValue: T = null as any) => {
+  const result = ref(defaultValue) as Ref<T>;
+  const loading = ref(false);
+  const error = ref<unknown>(null);
+  const run = async (...args: any) => {
+    loading.value = true;
+    error.value = null;
+    result.value = defaultValue;
+    try {
+      result.value = await fn(...args);
+    } catch (err) {
+      error.value = err;
+    } finally {
+      loading.value = false;
+    }
+  };
+  return { result, loading, error, run };
+};
