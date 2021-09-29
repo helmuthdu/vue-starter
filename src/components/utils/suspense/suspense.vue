d153e4f63fd35f88a38d0075775d545fa4a0ed02
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-09-29 19:23:23 +0200

    feat: added suspense component example

---
+++ src/components/utils/suspense/suspense.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="error">Uh oh .. {{ error }}</div>
+  <Suspense v-else>
+    <template #default> Some async component </template>
+    <template #fallback> Loading... </template>
+  </Suspense>
+</template>
+<script>
+  // import Event from '@/components/Event.vue';
+  import { onErrorCaptured, ref } from 'vue';
+
+  export default {
+    // components: { Event },
+    setup() {
+      const error = ref(null);
+      onErrorCaptured(e => {
+        error.value = e;
+        return true;
+      });
+      return { error };
+    }
+  };
+</script>
