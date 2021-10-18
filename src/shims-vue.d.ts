c43aa18184ef639de3a7eec6e46d6f4d799e0c76
helmuthdu <helmuthdu@gmail.com>
2021-10-18 21:07:36 +0200

    chore: minor change

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  import type { DefineComponent } from 'vue';
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
