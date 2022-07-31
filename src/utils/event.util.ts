88a5022a0be14371f1a9d1547248cd76143e7b3b
Helmuth Saatkamp <helmuthdu@gmail.com>
2022-07-31 08:30:50 +0200

    chore: update event bus util

--- src/utils/event.util.ts
+++ src/utils/event.util.ts
@@ -3,40 +3,63 @@ import { uuid } from './toolbox.util';
 
 const events: { [event: string]: { [id: string]: (arg?: any) => void } } = {};
 
-const on = (event: string, handler: (arg?: any) => void, options?: { once: boolean }) => {
+const getAll = (event: string) => events?.[event];
+
+const get = (event: string, id: string) => events?.[event]?.[id];
+
+const set = (event: string, id: string, fn: any) => {
+  if (!getAll(event)) events[event] = {};
+  events[event][id] = fn;
+};
+
+const stop = (event: string, id: string) => {
+  if (get(event, id)) {
+    delete events[event][id];
+    if (Object.keys(getAll(event)).length === 0) delete events[event];
+  } else {
+    Logger.warn(`Event "${event}" already stopped`);
+  }
+};
+
+const on = (event: string, fn: (arg?: any) => void) => {
   const id = uuid();
 
-  if (!events[event]) events[event] = {};
+  set(event, id, fn);
 
-  events[event][id] = options?.once
-    ? (arg?: any) => {
-        handler(arg);
-        stop();
-      }
-    : handler;
+  return { stop: () => stop(event, id) };
+};
 
-  const stop = () => {
-    if (events[event][id]) {
-      delete events[event][id];
-      if (Object.keys(events[event]).length === 0) delete events[event];
-    } else {
-      Logger.warn(`Event "${event}" already unsubscribed`);
-    }
-  };
+const once = (event: string, fn: (arg?: any) => void) => {
+  const id = uuid();
 
-  return stop;
+  set(event, id, (arg?: any) => {
+    fn(arg);
+    stop(event, id);
+  });
+
+  return { stop: () => stop(event, id) };
+};
+
+const off = (event: string) => {
+  if (getAll(event)) {
+    delete events[event];
+  } else {
+    Logger.warn(`All "${event}" events are already removed`);
+  }
 };
 
 const emit = (event: string, arg?: any) => {
-  if (!events[event]) {
+  if (!events || !events[event]) {
     Logger.warn(`Event "${event}" not registered`);
     return;
   }
 
-  Object.entries(events[event]).forEach(([_, handler]) => handler(arg));
+  Object.entries(getAll(event)).forEach(([_, fn]) => fn(arg));
 };
 
-export const eventuality = {
+export const Emitter = {
   on,
+  once,
+  off,
   emit
 };
