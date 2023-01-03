e9dd9df5bd964b0686eca2edce15ceb74bb095fe
Helmuth Saatkamp <helmuthdu@gmail.com>
2023-01-03 09:15:05 +0100

    chore: code cleanup

--- wallaby.config.js
+++ wallaby.config.js
@@ -3,6 +3,7 @@ module.exports = wallaby => {
   process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
 
   return {
+    autoDetect: true,
     files: ['src/**/*', '!src/**/__tests__/**/*.ts', 'jest.config.js', 'package.json', 'tsconfig.json'],
     tests: ['src/**/__tests__/**/*.ts'],
     env: {
