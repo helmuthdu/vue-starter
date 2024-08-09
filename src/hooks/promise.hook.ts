34db6d598bfa30dfd4b6bd586adeefc587a9a539
Helmuth Saatkamp <helmuthdu@gmail.com>
2024-08-09 08:51:09 +0200

    chore: update promise hook

--- src/hooks/promise.hook.ts
+++ src/hooks/promise.hook.ts
@@ -2,14 +2,25 @@ import { Logger } from '@/utils';
 import { type Ref, ref } from 'vue';
 
 export enum PromiseStatus {
-  PENDING = 'PENDING',
-  RESOLVED = 'RESOLVED',
-  REJECTED = 'REJECTED',
+  IDLE = 'idle',
+  PENDING = 'pending',
+  RESOLVED = 'resolved',
+  REJECTED = 'rejected',
 }
 
-export const usePromise = <T>(fn: (...args: unknown[]) => Promise<T>, defaultValue?: T) => {
+export function usePromise<T>() {
+  let resolve: (value: T | PromiseLike<T>) => void = () => {};
+  let reject: (reason?: unknown) => void = () => {};
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+  return { promise, resolve, reject };
+}
+
+export const usePledge = <T>(fn: (...args: unknown[]) => Promise<T>, defaultValue?: T) => {
   const value = ref(defaultValue) as Ref<T>;
-  const status = ref<PromiseStatus>();
+  const status = ref<PromiseStatus>(PromiseStatus.IDLE);
   const run = async (...args: unknown[]) => {
     try {
       status.value = PromiseStatus.PENDING;
