230a5023dfdb9e64fd1c6859504a2c279f6068b8
Helmuth Saatkamp <helmuthdu@gmail.com>
2025-01-26 23:14:03 +0100

    chore: update global store importer

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,28 @@
-import * as userModule from '../modules/user/stores';
+import appModules from '../modules';
+import * as notificationStore from './notification.store';
+
+export const stores = {
+  ...Object.values(appModules.stores).reduce(
+    (acc, store) => {
+      acc[store.name] = store.store;
+      return acc;
+    },
+    {} as {
+      [key in keyof typeof appModules.stores]: (typeof appModules.stores)[key]['store'];
+    },
+  ),
+  [notificationStore.name]: notificationStore.store,
+};
 
 export const useStore = () => ({
-  [userModule.user.name]: userModule.user.useStore(),
+  ...Object.values(appModules.stores).reduce(
+    (acc, store) => {
+      acc[store.name] = store.useStore();
+      return acc;
+    },
+    {} as {
+      [key in keyof typeof appModules.stores]: ReturnType<(typeof appModules.stores)[key]['useStore']>;
+    },
+  ),
+  [notificationStore.name]: notificationStore.useStore(),
 });
